7c06ce2a67fe72a03f98f8312ad29276e233d9c9
Rajesh R <90537663+rajeshreddyy@users.noreply.github.com>
2024-02-21 15:34:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+let questionsFormEl = document.getElementById("questionsForm");
+let submitBtnEl = document.getElementById("submitBtn");
+let resultMsgEl = document.getElementById("resultMsg");
+
+
+let cityHyderabadEl = document.getElementById("cityHyderabad");
+let cityChennaiEl = document.getElementById("cityChennai");
+let cityDelhiEl = document.getElementById("cityDelhi");
+let cityMumbaiEl = document.getElementById("cityMumbai");
+
+
+questionsFormEl.addEventListener("submit", function(event){
+    event.preventDefault();
+});
+
+let citySelected = "";
+cityHyderabadEl.addEventListener("change", function(event){
+    citySelected = event.target.value;
+    
+});
+cityChennaiEl.addEventListener("change", function(event){
+    citySelected = event.target.value;
+    
+});
+cityDelhiEl.addEventListener("change", function(event){
+    citySelected = event.target.value;
+    
+});
+cityMumbaiEl.addEventListener("change", function(event){
+    citySelected = event.target.value;
+    
+});
+
+submitBtnEl.addEventListener("click", function(){
+   if(citySelected === "Delhi"){
+       resultMsgEl.textContent = "Correct Answer!";
+       resultMsgEl.classList.add("text-success");
+   }
+   else if(citySelected === ""){
+       resultMsgEl.textContent = "Please select the Answer!";
+       resultMsgEl.classList.add("text-danger");
+   }
+   else{
+       resultMsgEl.textContent = "Wrong Answer!";
+       resultMsgEl.classList.add("text-danger");
+   }
+   
+});
+
+
+
+
